333923fc7ef0ab397f14a43281c3559d8845713d
Emanoel Vianna <emanoel.vianna@outlook.com>
2017-11-14 20:46:52 -0200

    Update Principal.java

--- src/main/java/br/com/principal/Principal.java
+++ src/main/java/br/com/principal/Principal.java
@@ -1,5 +1,7 @@
 package br.com.principal;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import org.cogroo.analyzer.Analyzer;
@@ -10,9 +12,13 @@ import org.cogroo.text.Token;
 import org.cogroo.text.impl.DocumentImpl;
 
 import br.com.utilitario.LerArquivoTexto;
+import br.com.utilitario.TermoRelevante;
 
 public class Principal {
+
 	public static void main(String[] args) {
+		List<TermoRelevante> maisRelevantes = new ArrayList<TermoRelevante>();
+
 		/* configurações de idioma */
 		ComponentFactory factory = ComponentFactory.create(new Locale("pt", "BR"));
 		Analyzer cogroo = factory.createPipe();
@@ -21,20 +27,27 @@ public class Principal {
 		LerArquivoTexto leitor = new LerArquivoTexto();
 		Document document = new DocumentImpl();
 
+		// TODO: adicionar os outros texto mais tarde!
+		
 		/* processando texto sobre esportes */
 		// document.setText(leitor.LerArquivoDeEsportes());
 		document.setText(leitor.LerArquivoDeTeste());
 		cogroo.analyze(document);
-
-		// TODO: adicionar os outros texto mais tarde!
 
 		/* lista de sentenças */
 		for (Sentence sentence : document.getSentences()) {
 			for (Token token : sentence.getTokens()) {
 				token.getLemmas(); // array com os possíveis lemas
 				if (token.getLemmas().length != 0) {
-					System.out.println(token.getLemmas()[0]);
-					System.out.println(token.getPOSTag());
+					/* classificar os termos mais relevantes */
+					if (!maisRelevantes.contains(token.getPOSTag())) {
+						TermoRelevante termo = new TermoRelevante();
+						termo.setTermo(token.getPOSTag());
+						maisRelevantes.add(termo);
+					} else {
+						int indexOf = maisRelevantes.indexOf(token.getPOSTag());
+						maisRelevantes.get(indexOf).repetiuTermo();
+					}
 				}
 			}
 		}
