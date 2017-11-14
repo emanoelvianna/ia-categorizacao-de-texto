b5db8a4c2e7b6249cf7058ab32b34bae846ea1f4
Emanoel Vianna <emanoel.vianna@outlook.com>
2017-11-14 20:30:29 -0200

    Create TermoRelevante.java

---
+++ src/main/java/br/com/utilitario/TermoRelevante.java
@@ -0,0 +1,26 @@
+package br.com.utilitario;
+
+public class TermoRelevante {
+	private String termo;
+	private Integer repeticao;
+
+	public TermoRelevante() {
+		this.repeticao = 0;
+	}
+
+	public String getTermo() {
+		return termo;
+	}
+
+	public void setTermo(String termo) {
+		this.termo = termo;
+	}
+
+	public Integer getRepeticao() {
+		return this.repeticao;
+	}
+
+	public void repetiuTermo() {
+		this.repeticao = this.repeticao++;
+	}
+}
