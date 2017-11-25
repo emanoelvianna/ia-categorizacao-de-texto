0a27ad596f2a0e93980ebb890ed33e3c616875c6
Mauricio Steinert <mauricio.steinert@gmail.com>
2017-11-25 09:54:49 -0200

    update

--- src/main/java/br/com/principal/Principal.java
+++ src/main/java/br/com/principal/Principal.java
@@ -17,23 +17,22 @@ public class Principal {
 		LerArquivoTexto lerArquivo = new LerArquivoTexto();
 		
 		/* Obtem lista de arquivos por aplicacao (treino/teste) */
-		//textos.addAll(lerArquivo.carregarTextos("textos/teste/treino", TipoTexto.TREINO, CategoriaTexto.POLICIA));
 		textos.addAll(lerArquivo.carregarTextos("textos/esporte/treino", TipoTexto.TREINO, CategoriaTexto.ESPORTE));
 		textos.addAll(lerArquivo.carregarTextos("textos/policia/treino", TipoTexto.TREINO, CategoriaTexto.POLICIA));
 		textos.addAll(lerArquivo.carregarTextos("textos/problema/treino", TipoTexto.TREINO, CategoriaTexto.PROBLEMA));
-		//textos.addAll(lerArquivo.carregarTextos("textos/trabalhador/treino", TipoTexto.TREINO, CategoriaTexto.TRABALHADOR));
+		textos.addAll(lerArquivo.carregarTextos("textos/trabalhador/treino", TipoTexto.TREINO, CategoriaTexto.TRABALHADOR));
 
 		/* Textos de testes */
-		//textos.addAll(lerArquivo.carregarTextos("textos/teste/teste", TipoTexto.TESTE, CategoriaTexto.POLICIA));
 		textos.addAll(lerArquivo.carregarTextos("textos/esporte/teste", TipoTexto.TESTE, CategoriaTexto.ESPORTE));
 		textos.addAll(lerArquivo.carregarTextos("textos/policia/teste", TipoTexto.TESTE, CategoriaTexto.POLICIA));
 		textos.addAll(lerArquivo.carregarTextos("textos/problema/teste", TipoTexto.TESTE, CategoriaTexto.PROBLEMA));
-		//textos.addAll(lerArquivo.carregarTextos("textos/trabalhador/teste", TipoTexto.TESTE, CategoriaTexto.TRABALHADOR));
+		textos.addAll(lerArquivo.carregarTextos("textos/trabalhador/teste", TipoTexto.TESTE, CategoriaTexto.TRABALHADOR));
 
 		try {
 			ProcessarTexto.gerarBOW(textos, 120, "output/treino.arff", "output/teste.arff");
 		} catch(Exception e) {
 			System.out.println("Falha ao criar arquivo de saida do BOW!");
+			e.printStackTrace();
 		}
 	}
 }
